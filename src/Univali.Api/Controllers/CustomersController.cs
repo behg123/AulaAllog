f0699dd416c4fc944f738ce6f1c6a791176fde77
behg123 <85688122+behg123@users.noreply.github.com>
2023-05-26 17:20:00 -0300

    Get User By Id and CPF

--- src/Univali.Api/Controllers/CustomersController.cs
+++ src/Univali.Api/Controllers/CustomersController.cs
@@ -10,11 +10,41 @@ public class CustomersController : ControllerBase
     [HttpGet]
     public ActionResult<IEnumerable<Customer>> GetCustomers()
     {
-
         var result = Data.instanceAcess().Customers;
-
-
         return Ok(result);
     }
+
+    [HttpGet("id/{id}")]
+    public ActionResult<Customer> GetCustomerById([FromRoute] int id)
+    {
+        var result = Data.instanceAcess().Customers.FirstOrDefault(c => c.Id == id);
+
+
+        return result != null ? Ok(result) : NotFound();
+    }    
+    
+    [HttpGet("cpf/{cpf}")]
+    public ActionResult<Customer> GetCustomerByCpdf([FromRoute] string cpf)
+    {
+        var result = Data.instanceAcess().Customers.FirstOrDefault(c => c.Cpf == cpf);
+
+
+        return result != null ? Ok(result) : NotFound();
+    }
+    [HttpPost]
+    public ActionResult<Customer> CreateCustomer([FromBody] Customer customer)
+    {
+        var newCustomer = new Customer
+        {
+            Id = Data.instanceAcess().Customers.Max(c => c.Id) + 1,
+            Name = customer.Name,
+            Cpf = customer.Cpf
+
+        };
+
+        Data.instanceAcess().Customers.Add(newCustomer);
+        return Ok(newCustomer);
+    }
+    
 }
 
